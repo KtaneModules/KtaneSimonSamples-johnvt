deaa2275070ee58e6e6a880abeae9ae70ad0ae16
John van Terheijden <john@fastguide.eu>
2018-06-28 16:47:32 +0200

    Add play and record buttons.

--- Assets/SimonSamples.cs
+++ Assets/SimonSamples.cs
@@ -11,52 +11,91 @@ public class SimonSamples : MonoBehaviour
     public KMBombInfo Bomb;
     public KMSelectable Module;
     public KMAudio Audio;
-    public KMSelectable[] Pads;
     public KMSelectable PlayButton;
+    public KMSelectable RecordButton;
+    public KMSelectable[] Pads;
 
     private int _moduleId;
     private static int _moduleIdCounter = 1;
 
-    private string[] _sounds = { "HiHat", "Kick", "OpenHiHat", "Snare" };
+    private string[] _sounds = { "Kick", "Snare", "HiHat", "OpenHiHat" };
     private KMAudio.KMAudioRef _audioRef;
     private bool _isPlaying;
+    private bool _isRecording;
+    private Queue<int> _objective = new Queue<int>();
+    private List<List<List<int>>> _possibleObjectives = new List<List<List<int>>>() {
+
+        // First stage
+        new List<List<int>>()
+        {
+            new List<int>("0212".Select(s => (int)(s)).ToArray()),
+            new List<int>("0213".Select(s => (int)(s)).ToArray()),
+            new List<int>("0012".Select(s => (int)(s)).ToArray()),
+            new List<int>("0112".Select(s => (int)(s)).ToArray()),
+        }
+    };
 
     void Start()
     {
         _moduleId = _moduleIdCounter++;
 
+        PlayButton.OnInteract += delegate () { PressPlay(); return false; };
+        RecordButton.OnInteract += delegate () { PressRecord(); return false; };
+
         for (int i = 0; i < Pads.Length; i++)
         {
             var j = i;
-            Pads[i].OnInteract += delegate () { PressButton(j); return false; };
+            Pads[i].OnInteract += delegate () { HitPad(j); return false; };
         }
 
-        PlayButton.OnInteract += delegate () { PressPlayButton(); return false; };
-
+        _objective = new Queue<int>(new[] { 0, 2, 1, 2, 0, 3, 1, 2 });
     }
 
-    private void PressPlayButton()
+    private void PressPlay()
     {
-        StartCoroutine(PlayRhythm());
-    }
+        GetComponent<KMSelectable>().AddInteractionPunch(.1f);
 
-    private IEnumerator PlayRhythm()
-    {
-        if (_isPlaying) yield break;
+        if (_isPlaying || _isRecording) return;
+
         _isPlaying = true;
-
-        var startTime = (int)Bomb.GetTime();
-        var time = startTime;
-        while (time == startTime)
-        {
-            yield return null;
-            time = (int)Bomb.GetTime();
-        }
-        PlaySound(0);
-        _isPlaying = false;
+        SetLed(PlayButton, true);
+        StartCoroutine(PlayRhythm(new Queue<int>(new [] { 0, 2, 1, 2, 0, 3, 1, 2 })));
     }
 
-    private void PlaySound(int i)
+    private void PressRecord()
+    {
+        GetComponent<KMSelectable>().AddInteractionPunch(.1f);
+
+        if (_isPlaying || _isRecording) return;
+
+        _isRecording = true;
+        SetLed(RecordButton, true);
+    }
+
+    private IEnumerator PlayRhythm(Queue<int> tones)
+    {
+        var startTime = Bomb.GetTime();
+        while ((int)Bomb.GetTime() == (int)startTime) yield return null;
+        startTime = Bomb.GetTime();
+        var tonesPerTick = 2;
+
+        var nextToneTime = startTime;
+        while (tones.Count > 0)
+        {
+            PlayTone(tones.Dequeue());
+            nextToneTime = nextToneTime - (1f / tonesPerTick);
+            while (Bomb.GetTime() > nextToneTime)
+            {
+                yield return null;
+            }
+        }
+
+        SetLed(PlayButton, false);
+        _isPlaying = false;
+        yield return null;
+    }
+
+    private void PlayTone(int i)
     {
         if (_audioRef != null)
         {
@@ -74,8 +113,35 @@ public class SimonSamples : MonoBehaviour
         }
     }
 
-    private void PressButton(int i)
+    private void HitPad(int i)
     {
-        PlaySound(i);
+        GetComponent<KMSelectable>().AddInteractionPunch();
+        PlayTone(i);
+
+        if (_isRecording)
+        {
+            if (i == _objective.Peek())
+            {
+                _objective.Dequeue();
+                if (_objective.Count == 0)
+                {
+                    _isRecording = false;
+                    SetLed(RecordButton, false);
+                }
+            }
+            else
+            {
+                GetComponent<KMBombModule>().HandleStrike();
+                _isRecording = false;
+                SetLed(RecordButton, false);
+                // to do: reset objective
+            }
+        }
+    }
+
+    private void SetLed(KMSelectable selectable, bool on)
+    {
+        selectable.transform.Find("LedOn").gameObject.SetActive(on);
+        selectable.transform.Find("LedOff").gameObject.SetActive(!on);
     }
 }
